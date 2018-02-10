7ed8002ba24aa14a7c37cfb65548c290323db591
Blimbo <33764345+Blimbo@users.noreply.github.com>
2018-02-10 21:06:48 +0200

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -2,7 +2,7 @@
   var byeSpeaker = {};
   var speakWord = "Good Bye";
   byeSpeaker.speak = function (name) {
-    сonsole.log(speakWord + " " + name);
+    console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
 })(window);
